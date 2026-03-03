6fb1201ad064badf69b7e909073b4fd24ff47aaa
정찬훈 <ckohoon@gmail.com>
2026-03-03 23:24:09 +0900

    feat: curriculum.js 생성

---
+++ curriculum.js
@@ -0,0 +1,32 @@
+const CURRICULUM = [
+    {
+        title: "생성형 AI 혁신",
+        description: "AI 리터러시 및 영업 생산성 고도화",
+        link: "https://drive.google.com/file/d/1X3PyswCDGMBe6AGUEOyOpsXbM3pG_kxD/view?usp=sharing",
+        order: 1
+    },
+    {
+        title: "B2B 실전 영업",
+        description: "B2B 영업 혁신 및 가치 제안 전략",
+        link: "https://drive.google.com/file/d/1HxriQxRTEpADeKllhj4AnYQ29tdVDCk6/view?usp=sharing",
+        order: 2
+    },
+    {
+        title: "워크스마트",
+        description: "업무 생산성 및 스마트 업무 관리",
+        link: "https://drive.google.com/file/d/1iSEOHH1-Kp4r6ZGJEEJf1E5DT0zSOmfv/view?usp=sharing",
+        order: 3
+    },
+    {
+        title: "기업 재무설계",
+        description: "생애 주기별 맞춤형 자산 관리 로드맵",
+        link: "https://drive.google.com/file/d/1nm-6-gEyOoLccPb7q54n5mNqEYf-KH0B/view?usp=sharing",
+        order: 4
+    },
+    {
+        title: "제약·의료기기 영업",
+        description: "제약·의료기기 산업 특화 영업 전략",
+        link: "https://drive.google.com/file/d/1MU-m5KLP8M96Tp_5Sop0RA50xT_MXtjg/view?usp=sharing",
+        order: 5
+    }
+];
